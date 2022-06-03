4d4d3c483c326e0eef32c26ccbf466a222c99f34
ingaz4vili <ingaz4vili@gmail.com>
2022-06-03 12:50:52 +0200

    Done

--- task_0_10.py
+++ task_0_10.py
@@ -1,6 +1,10 @@
 def common_letters(s1,s2):
-    letters=list(set(s1.lower())&set(s2.lower()))
+    str1=s1.lower()
+    str2=s2.lower()
+    letter = list()
     print("Common letters:",end='')
-    for i in letters:
-        print(f'',i,end=' ,')
+    for i in str2:
+        if  i in str1:
+           letter.append(i) 
+    print(','.join(letter),end='')
 common_letters("house","COMPUTERS")
--- task_0_9.py
+++ task_0_9.py
@@ -1,5 +1,12 @@
+from ntpath import join
+
+
 def vowels_string(text):
-    vowels = "aeiuo" 
-    result = [letter for letter in text.lower() if letter in vowels]
-    print("Vowels:",(result))
+    vowels = "aeiuo"
+    result = list()
+    for letter in text: 
+        ln = letter.lower()
+        if ( ln in vowels ) and (ln not in result ):
+            result.append(ln)
+    print(','.join(result))
 vowels_string("Umuzi")
